462fc135b240cbd506c1b599d668f8c500f10be4
KhatunaKhatuna <kh.chitishvili@gmail.com>
2024-02-20 23:47:03 +0400

    Create step component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Card from "./components/Card";
+import Steps from "./components/Steps";
 function App() {
   return (
     <div className="App">
       <Card />
+      <Steps />
     </div>
   );
 }
