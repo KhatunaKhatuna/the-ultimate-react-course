5451b5a1c969af8369e4fc9f1b4a53fd7da0767d
KhatunaKhatuna <kh.chitishvili@gmail.com>
2024-02-21 00:09:42 +0400

    minor changes

--- src/components/Steps.js
+++ src/components/Steps.js
@@ -22,28 +22,33 @@ export default function Steps() {
   }
   return (
     <div className="container">
-      <div className="steps">
-        <div className="numbers">
-          <div className={step >= 1 ? "active" : ""}>1</div>
-          <div className={step >= 2 ? "active" : ""}>2</div>
-          <div className={step >= 3 ? "active" : ""}>3</div>
-        </div>
-        <p className="message">
-          Step {step}: {messages[step - 1]}
-        </p>
-        <div className="buttons">
-          <button
-            style={{ backgroundColor: "#7950f2", color: "#fff" }}
-            onClick={hendlePrevious}
-          >
-            Prtevious
-          </button>
-          <button
-            style={{ backgroundColor: "#7950f2", color: "#fff" }}
-            onClick={hendleNext}
-          >
-            Next
-          </button>
+      <div>
+        <h2 className="challenge" style={{ marginBottom: "100px" }}>
+          Challenge 2
+        </h2>
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={hendlePrevious}
+            >
+              Prtevious
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={hendleNext}
+            >
+              Next
+            </button>
+          </div>
         </div>
       </div>
     </div>
