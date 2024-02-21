2b60c704791a5ed893bc414e599502cb203b626a
KhatunaKhatuna <kh.chitishvili@gmail.com>
2024-02-21 11:36:32 +0400

    Add another state for conditional rendering

--- src/components/Steps.js
+++ src/components/Steps.js
@@ -8,6 +8,7 @@ const messages = [
 
 export default function Steps() {
   const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState("true");
 
   function hendlePrevious() {
     if (step > 1) {
@@ -21,35 +22,40 @@ export default function Steps() {
     }
   }
   return (
-    <div className="container">
+    <div className="container" style={{ position: "relative" }}>
       <div>
         <h2 className="challenge" style={{ marginBottom: "100px" }}>
           Challenge 2
         </h2>
-        <div className="steps">
-          <div className="numbers">
-            <div className={step >= 1 ? "active" : ""}>1</div>
-            <div className={step >= 2 ? "active" : ""}>2</div>
-            <div className={step >= 3 ? "active" : ""}>3</div>
+        <button className="close" onClick={() => setIsOpen(!isOpen)}>
+          &times;
+        </button>
+        {isOpen && (
+          <div className="steps">
+            <div className="numbers">
+              <div className={step >= 1 ? "active" : ""}>1</div>
+              <div className={step >= 2 ? "active" : ""}>2</div>
+              <div className={step >= 3 ? "active" : ""}>3</div>
+            </div>
+            <p className="message">
+              Step {step}: {messages[step - 1]}
+            </p>
+            <div className="buttons">
+              <button
+                style={{ backgroundColor: "#7950f2", color: "#fff" }}
+                onClick={hendlePrevious}
+              >
+                Prtevious
+              </button>
+              <button
+                style={{ backgroundColor: "#7950f2", color: "#fff" }}
+                onClick={hendleNext}
+              >
+                Next
+              </button>
+            </div>
           </div>
-          <p className="message">
-            Step {step}: {messages[step - 1]}
-          </p>
-          <div className="buttons">
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={hendlePrevious}
-            >
-              Prtevious
-            </button>
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={hendleNext}
-            >
-              Next
-            </button>
-          </div>
-        </div>
+        )}
       </div>
     </div>
   );
